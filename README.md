3d1c020d82b3b84fba24d672be0794aff5747b6b
David Sena <sena.ufc@gmail.com>
2016-09-10 10:52:40 -0300

    up

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # test
+
+#### menor_vetor
+Leia um vetor de inteiros e imprima o menor valor{
+[C](codes/menor_vetor.c),
+[CPP](codes/menor_vetor.cpp), [Python](codes/menor_vetor.py)
+}
---
+++ codes/menor_vetor.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+	cout << "Digite valores inteiros e -1 para sair" << endl;
+	int value = 0;
+    vector<int> valores;
+	while(true){
+        cin >> value;
+        if(value == -1)
+            break;
+        valores.push_back(value);
+    }
+    int menor = valores[0];
+    for(auto valor : valores){
+        if(valor < menor){
+            menor = valor;
+        }
+    }
+    cout << "O menor eh " << menor << endl;
+    return 0;
+}
+
+
+
+			
