266afba9ffce6cf45dad104da44a596a9576e457
David Sena <sena.ufc@gmail.com>
2021-10-13 11:39:22 -0300

    testes 1

--- respostas/testador.hpp
+++ respostas/testador.hpp
@@ -2,28 +2,28 @@
 
 #include <iostream>
 #include <vector>
+#include <sstream>
 
 template <typename T>
-void testar(const T& A, const T& B) {
-    if (A == B)
+std::string to_string(T value) {
+    std::ostringstream os;
+    os << value;
+    return os.str();
+}
+
+template <typename T, typename K>
+void compare(const T& A, const K& B) {
+    if (to_string(A) == to_string(B))
         std::cout << "\tok\n";
     else
         std::cout << '\t' << A << " != " << B << "\n";
 }
 
-std::ostream& operator<<(std::ostream& os, const std::vector<int>& v) {
-    os << "[ ";
-    for (auto i : v)
-        os << i << " ";
-    os << "]";
+template <class T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) {
+    os << "{";
+    for (int i = 0; i < (int) v.size(); i++)
+        os << v[i] << (i + 1 < (int) v.size() ? ", " : "");
+    os << "}";
     return os;
-}
-
-bool operator==(const std::vector<int>& A, const std::vector<int>& B) {
-    if (A.size() != B.size())
-        return false;
-    for (int i = 0; i < (int) A.size(); i++)
-        if (A[i] != B[i])
-            return false;
-    return true;
 }
